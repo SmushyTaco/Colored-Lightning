66b1f4063cf072fb1586c24891f84e89e56b3e79
Nikan Radan <github.com@alias.nikanradan.com>
2024-12-16 23:20:31 -0800

    Update ColoredLightning.kt

--- Colored Lightning/src/main/kotlin/com/smushytaco/colored_lightning/ColoredLightning.kt
+++ Colored Lightning/src/main/kotlin/com/smushytaco/colored_lightning/ColoredLightning.kt
@@ -1,7 +1,5 @@
 package com.smushytaco.colored_lightning
-import kotlinx.coroutines.*
 import net.fabricmc.api.ClientModInitializer
-import java.util.*
 object ColoredLightning : ClientModInitializer {
     const val MOD_ID = "colored_lightning"
     val config = ModConfig.createAndLoad()
